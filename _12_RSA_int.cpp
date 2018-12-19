336ddfe147840f72e72aa65e93df16a8cd911b57
Szymon <45321078+Szymon300101@users.noreply.github.com>
2018-12-19 22:36:56 +0100

    Add files via upload

---
+++ _12_RSA_int.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <conio.h>
+#include <random>
+#include <chrono>
+#include <cstring>
+
+//Range of p - main seed of RSA
+//pMAX shouldn't be more than 1000 due to
+//stability issues (in theory it could be higher)
+#define pMIN 100
+#define pMAX 999
+
+//approximate difference between p1 and p2
+//better no more than half of pMAX-pMIN
+#define pSENS 100
+
+//starting value of 'e' parameter.
+//3 is optimal, have to be small prime number
+#define e0 3
+
+using namespace std;
+
+//Function generating random number using "Random" library.
+//Unfortunately there must be that third argument,
+//witch allows to make following seeds differ (library don't provide it).
+unsigned int rGen(unsigned int, unsigned int, int&);
+
+//Function generating prime number, using rGen() as a base.
+unsigned int rGenPrime(unsigned int, unsigned int, int&);
+
+//Function returning Modular Power of given numbers.
+//It uses specific algorithm, calculating it on small
+//numbers, even for big input.
+long long PowMod(long long, long long, long long);
+
+//Main function for USER Alice.
+//She generates keys of the code,
+//and decrypts message at the end.
+void Alice()
+
+//Main function for USER Bob.
+//He gets keys, encrypts his secret message,
+//sends it to Alice
+void Bob()
+
+//*************************************************************//
+//Main code
+int main()
+{
+    while(1)
+    {
+        char chose;
+        cout<<"0 - ALLICE"<<endl<<"1 - BOB"<<endl; chose=getch();
+        if(chose=='0')
+            Alice();
+        else
+            Bob();
+        getch();
+        cout<<endl<<endl<<"<--------------------------------------------------------->"<<endl;
+    }
+}
+
+void Alice()
+{
+    int randC=0;    //variable to vary generation seeds
+    string t;
+    long long p1=rGenPrime(pMIN,pMAX,randC);                //1 part of n
+    long long p2=rGenPrime(p1-pSENS,p1+pSENS,randC);        //2 part of n
+        if(p1==p2) p2=rGenPrime(p2-pSENS,p2+pSENS,randC);
+    long long n=p1*p2;  //making n key
+    long long fiN=(p1-1)*(p2-1);    //Euler's function (fi) of n
+    long long e=e0;         //making e key
+        while(fiN%e==0) e+=2;
+    long long k=1;              //making d key (Private)
+    long long d=(k*fiN+1)/e;
+    while((long long)(k*fiN+1)%e!=0)
+    {
+        k++;
+        d=(k*fiN+1)/e;
+        if(k>50)
+        {   e+=2;
+            k=1;
+        }
+    }
+    //cout<<"p1= "<<p1;             //some hidden prints, for debug or looking into process
+    //cout<<"; p2= "<<p2<<endl;
+    cout<<endl<<"n= "<<n<<endl;
+    //cout<<"fi(n)= "<<fiN<<endl;
+    cout<<"e= "<<e<<endl;
+    //cout<<"k= "<<k<<endl;
+    //cout<<"d= "<<d<<endl;
+    cout<<"Insert c: "; cin>>t;
+    int mult=0;                     //my original algorithm witch let user to send
+    if(t.find('.',0)!=-1)           //messages bigger than n.
+    {
+        int f=t.find('.',0);
+        string in;
+        for(int i=0;i<f;i++)
+            in+=t[i];
+        mult=stoi(in);
+        t.erase(0,f+1);
+    }
+    long long c;
+    c=stoi(t);
+    long long m=PowMod(c,d,n);
+    m+=n*mult;
+    cout<<"Received message: "<<m;
+}
+
+void Bob()
+{
+    int randC=0;            //same as higher
+    string t;
+    long long n;            //getting public keys
+    cout<<"Insert n: "; cin>>n;
+    long long e;
+    cout<<"Insert e: "; cin>>e;
+    long long m;
+    cout<<"Your message: "; cin>>m;
+    int cnt=0;              //sending part of my algorithm.
+    while(m>n)
+    {
+        m-=n;
+        cnt++;
+    }
+    t="";
+    long long c=PowMod(m,e,n);
+    if(cnt>0)
+    {
+        t=to_string(cnt);
+        t+='.';
+    }
+    t+=to_string(c);
+    cout<<"c="<<t<<endl;
+}
+
+unsigned int rGen(unsigned int l, unsigned int h, int &c)
+{
+    c++;
+    default_random_engine generator(chrono::system_clock::now().time_since_epoch().count()/c*1.0);
+    uniform_int_distribution<unsigned int> distribution(l,h);
+    return distribution(generator);
+}
+
+unsigned int rGenPrime(unsigned int l, unsigned int h, int &c)
+{
+    unsigned int prime = rGen(l,h-2,c);
+    bool flag=1;
+    while(flag==1 || prime==1)
+    {
+        prime--;
+        int sq = sqrt(prime);
+        flag=0;
+        int a=2;
+        while(a<=sq && flag==0)
+        {
+            if(prime%a==0) flag=1;
+            a++;
+        }
+    }
+    return prime;
+}
+
+long long PowMod(long long a, long long w, long long n)
+{
+long long pot,wyn,q;
+
+  pot = a; wyn = 1;
+  for(q = w; q > 0; q /= 2)
+  {
+    if(q%2!=0) wyn = (wyn * pot) % n;
+    pot = (pot * pot) % n;
+  }
+  return wyn;
+}
